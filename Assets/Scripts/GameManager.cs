261656f2c624aeb4f7778a08440b3158359c4a27
Gabriel Cavalcante <gabrielcavalcante1021@gmail.com>
2020-04-17 18:58:10 -0300

    Menus e Quadro Negro
    Todos os botões e menus no lugar.
    Falta a mecânica principal do jogo e o background.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,16 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public int pontuacao;
+    //Variável para mostrar a pontuação na tela de jogo
+    public Text textPontuacao;
+    // Variável para mostrar a pontuação final na tela de game over
+    public Text textPontuacaoFinal;
+    public bool jogoPausado;
+    public GameObject optionPanel;
+    // Mostra painel de Game over
+    public GameObject gameOverPanel;
+    public GameObject quadro;
     public GameObject menuInicial;
     public GameObject menuCredito;
 
@@ -14,15 +24,52 @@ public class GameManager : MonoBehaviour
         // Variável para a pontuação
         pontuacao = 0;
         // Mostra o resultado do jogador na caixa de texto
-        textPontuacao.text = pontuacao.ToString();
+        //textPontuacao.text = pontuacao.ToString();
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            janelaOpcoes();
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+
+        }
+    }
+
+    // Aumenta a pontuação do jogador.
+    public void aumentaPontuacao()
+    {
+        pontuacao++;
+        textPontuacao.text = pontuacao.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // Método para iniciar jogo
+    public void iniciarJogo()
+    {
+        //SceneManager.LoadScene(1);
+    }
+
+    // Método para reiniciar jogo
+    public void reiniciarJogo(){
+        //SceneManager.LoadScene(1);
+        Time.timeScale = 1f;
+    }
+
+    public void mostrarQuadro()
+    {
+        menuInicial.SetActive(false);
+        menuCredito.SetActive(false);
+        quadro.SetActive(true);
     }
 
     public void abrirCreditos()
@@ -35,5 +82,11 @@ public class GameManager : MonoBehaviour
     {
         menuInicial.SetActive(true);
         menuCredito.SetActive(false);
+    }
+
+    //Método para sair do jogo.
+    public void sairDoJogo(){
+        Debug.Log("Clicou em SAIR");
+        Application.Quit();
     }
 }
